8f5c28fe88a70e91cee278ae0e73eb8629d3c3ce
Abhay Kumar Ray <abhayray2002@gmail.com>
2022-03-24 10:50:10 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+
+const app = express();
+app.set('view engine', 'ejs');
+
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+    res.render('home');
+})
+
+app.get('/aboutus', function(req, res) {
+    res.render('aboutus');
+})
+
+app.get('/learner', function(req, res) {
+    res.render('learner');
+})
+
+app.get('/college', function(req, res) {
+    res.render('college');
+})
+
+app.get('/company', function(req, res) {
+    res.render('company');
+})
+
+app.get('/compiler', function(req, res) {
+    res.render('compiler');
+})
+
+app.get('/login', function(req, res) {
+    res.render('login');
+})
+
+app.get('/registration', function(req, res) {
+    res.render('registration');
+})
+
+app.listen(8080);
