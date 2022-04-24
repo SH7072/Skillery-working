5c9226197c90259a016c5fbd43193dd2a0ca84e2
Shubham Chandwani <chandwanishubham@gmail.com>
2022-04-24 22:41:31 +0530

    course model

--- model/course.js
+++ model/course.js
@@ -2,25 +2,15 @@ const {Schema,model} = require('mongoose')
 
 const CourseModel=new Schema({
     name:String,
-    week:[{
-        type:Schema.Types.ObjectId,
-        ref:'Week'
+    weeks:[{
+        done:Number,
+        days:[{
+            lectureLink:String,
+            homeworkLink:String,
+            score:Number
+        }]
     }]
 })
 
-const WeekSchema=new Schema({
-    done:Number,
-    days:[{
-        type:Schema.Types.ObjectId,
-        ref:'Day'
-    }]
-})
 
-const DaySchema=new Schema({
-    lectureLink:String,
-    homeworkLink:String,
-    score:Number
-})
-
-
-module.exports = {Day:model('Day',DaySchema),Week:model('Week',WeekSchema),Course:model('Course',CourseModel)}
+module.exports = {Course:model('Course',CourseModel)}
